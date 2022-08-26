7d55e384684e94ab89f43b4b7c50c6894ffba9d4
Eleanor Ruth <ellie.ruth@me.com>
2022-08-25 20:55:10 -0600

    Add functions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return "The " + bookIdea
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+}
+
+function saveReview(reviewText, reviews) {
+  if (reviews.includes(reviewText) === false) {
+    return reviews.push(reviewText)
+  }
+}
+
+
+function calculatePageCount(bookTitle, bookPageCount) {
+  var bookPageCount = bookTitle.length  * 20
+  return bookPageCount;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookTitle.length  * 20,
+    genre: genre,
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
