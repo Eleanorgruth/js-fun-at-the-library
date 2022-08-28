1cbeea91869e25fb7eab0295d08bf32ad1861787
Eleanor Ruth <ellie.ruth@me.com>
2022-08-27 19:15:16 -0600

    Add functions

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,45 @@
+function createLibrary(nameOfLib) {
+  var libraryObject = {
+    name: nameOfLib,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return libraryObject
+}
+
+function addBook(libraryName, bookObject) {
+  if (bookObject.genre === "fantasy") {
+    libraryName.shelves.fantasy.push(bookObject)
+  } else if (bookObject.genre === "fiction") {
+    libraryName.shelves.fiction.push(bookObject)
+  } else if (bookObject.genre === "nonFiction") {
+    libraryName.shelves.nonFiction.push(bookObject)
+  }
+  return bookObject
+}
+
+function checkoutBook(libraryLocation, bookName, bookGenre) {
+var shelf = libraryLocation.shelves[bookGenre]
+var isOnShelf = false
+  for (var i = 0; i < shelf.length; i++) {
+    if (bookName === shelf[i].title) {
+      shelf.splice(i, 1)
+      isOnShelf = true
+    }
+    }
+    return isOnShelf
+    ? `You have now checked out ${bookName} from the ${libraryLocation.name}`
+    : `Sorry, there are currently no copies of ${bookName} available at the ${libraryLocation.name}`
+
+  }
 
 
+//bracket notation - if you don't know the exact property[]
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
